ba16eddec304a2b9e92ed91b78adb02ac9fbb875
yuriysyt <yyurchikoff@gmail.com>
2025-03-20 10:20:00 +0000

    Models: Added HealthCheckCard model for tracking metrics

--- models.py
+++ models.py
@@ -8,3 +8,11 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
+class HealthCheckCard(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.CharField(max_length=50)
+    active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
